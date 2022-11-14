18ccd8da917498acaac4560e4d7130906434a7f9
Nima Noorali <96994243+mynameisNN2@users.noreply.github.com>
2022-11-14 14:25:23 +0330

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+        'X-RapidAPI-Key': '6e098ba400msh547cf710dea8f38p1ebd95jsnde90bf747f2c',
+        "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "71565df9bbffa8f8f92a5dd43145b172";
